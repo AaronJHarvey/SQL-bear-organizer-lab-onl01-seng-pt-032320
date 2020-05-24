1046e523b83e8bffa597f1b289396f6d72020235
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-05-24 22:48:12 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,15 +15,15 @@ def selects_oldest_bear_and_returns_name_and_age
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "SELECT name, age FROM bears ORDER BY age ASC LIMIT 1"
+  "SELECT name, age FROM bears ORDER BY age ASC LIMIT 1;"
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "Write your SQL query here"
+  "Write your SQL query here;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT COUNT(temperaments)"
+  "SELECT COUNT(temperaments) FROM bears WHERE temperament = 'goofy';"
 end
 
 def selects_bear_that_killed_Tim
